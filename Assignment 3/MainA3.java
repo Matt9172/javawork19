3bf5ee52459d7627d492f70f117df0acfb11d5fb
Matt9172 <bigmack4@hotmail.co.uk>
2019-12-16 21:47:25 +0000

    Assignment 3 Started

--- Assignment 3/MainA3.java
+++ Assignment 3/MainA3.java
@@ -1,20 +1,19 @@
-import java.text.DecimalFormat;
-import java.math.RoundingMode;
 import java.text.NumberFormat;
 import java.util.Locale;
 public class MainA3 {
-    private static DecimalFormat df2 = new DecimalFormat("##.##");
 
     public static void main(String[] args) {
-        df2.setRoundingMode(RoundingMode.UP);
+
+        Locale locale = new Locale ("en", "GB");
+        NumberFormat cf = NumberFormat.getCurrencyInstance (locale);
         CrackerPacker matt = new CrackerPacker("Matt", 200, 10);
         CrackerPacker dave = new CrackerPacker("Dave", 1200, 60);
 
         System.out.println(matt.getName());
         System.out.println(matt.getNumberOfBoxes());
         System.out.println(matt.getWage());
-        System.out.println("£" + matt.finalWage());
-        System.out.println("£" + df2.format(matt.improvedFinalWage()));
-        System.out.println("£" + df2.format(dave.improvedFinalWage()));
+        System.out.println(matt.finalWage());
+        System.out.println(cf.format(matt.improvedFinalWage()));
+        System.out.println(cf.format(dave.improvedFinalWage()));
     }
 }
