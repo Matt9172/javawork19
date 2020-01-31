6db6b24d6d0947d4492ffbf50cef0c1a23fb8469
Matthew McDougall <U1961826@unimail.hud.ac.uk>
2020-01-31 17:01:41 +0000

    Term 2

--- Term2/Week2/RugbyTeam.java
+++ Term2/Week2/RugbyTeam.java
@@ -1,6 +1,22 @@
 package Week2;
 
 public class RugbyTeam {
-    public String teamName;
+    private String teamName;
+    private int won;
+    private int lost;
+    private int drawn;
+    private int scored;
+    private int against;
+
+    public RugbyTeam(String teamName, int won, int lost, int drawn, int scored, int against) {
+        this.teamName = teamName;
+        this.won = won;
+        this.lost = lost;
+        this.drawn = drawn;
+        this.scored = scored;
+        this.against = against;
+    }
+
+
 
 }
--- Term2/Week2/SuperLeagueTable.java
+++ Term2/Week2/SuperLeagueTable.java
@@ -2,4 +2,9 @@ package Week2;
 
 public class SuperLeagueTable {
 
+    public static void main(String[] args) {
+        RugbyTeam cas = new RugbyTeam ("Castleford Tigers", 1,0,0,10,4  );
+
+    }
+
 }
