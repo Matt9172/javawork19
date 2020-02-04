ebea01d0d92784f451e914ca8a19752dc0606cbb
Matthew McDougall <U1961826@unimail.hud.ac.uk>
2020-02-04 15:31:51 +0000

    Term 2

--- Term2/Week2/RugbyTeam.java
+++ Term2/Week2/RugbyTeam.java
@@ -5,18 +5,43 @@ public class RugbyTeam {
     private int won;
     private int lost;
     private int drawn;
-    private int scored;
+    private int score;
     private int against;
 
-    public RugbyTeam(String teamName, int won, int lost, int drawn, int scored, int against) {
+    public RugbyTeam(String teamName, int won, int lost, int drawn) {
         this.teamName = teamName;
         this.won = won;
         this.lost = lost;
         this.drawn = drawn;
-        this.scored = scored;
-        this.against = against;
+
     }
 
+    public int getWon() {
+        return won;
+    }
 
+    public void playGame (int t1, int t2){
+        if (t1 > t2) {
+            won =+1;
+        }
+        else if (t2 > t1){
+            lost =+1;
+        }
+        else {
+            drawn =+1;
+        }
+    }
 
+    public String getScore (){
+        score = ((won * 2)+(drawn));
+        return teamName + " has " + score + " points! " ;
+    }
+
+    @Override
+    public String toString() {
+        return "Team Name: '" + teamName + '\'' +
+                ", Won: " + won +
+                ", Lost: " + lost +
+                ", Drawn: " + drawn;
+    }
 }
--- Term2/Week2/SuperLeagueTable.java
+++ Term2/Week2/SuperLeagueTable.java
@@ -3,8 +3,16 @@ package Week2;
 public class SuperLeagueTable {
 
     public static void main(String[] args) {
-        RugbyTeam cas = new RugbyTeam ("Castleford Tigers", 1,0,0,10,4  );
+        RugbyTeam cas = new RugbyTeam ("Castleford Tigers", 0,0,0 );
+        RugbyTeam leeds = new RugbyTeam("Leeds Rhinos", 0,0,0);
+        System.out.println(cas.getScore());
+        cas.playGame(20, 10);
+        cas.playGame(10,12);
+        cas.playGame(16,16);
 
+
+        System.out.println(cas);
+        System.out.println(cas.getScore());
     }
 
 }
