830f8280d4568ed2d7c46beac106bc71661e3c6f
Matthew McDougall <U1961826@unimail.hud.ac.uk>
2020-02-20 14:08:26 +0000

    Term 2

--- Term2/Week4/CurrentAccount.java
+++ Term2/Week4/CurrentAccount.java
@@ -1,75 +1,9 @@
 package Week4;
 
-import java.util.Random;
-
-public class CurrentAccount {
-    private String accountNumber;
-    private String accountHolder;
-    private double balance;
-    private boolean hasOverdraft;
 
 
-    public String getAccountNumber() {
-        return accountNumber;
-    }
-
-    public void setAccountNumber() {
-        this.accountNumber = generateAccountNumber();
-    }
-
-    private String generateAccountNumber  (){
-        String acc = "";
-        Random rg = new Random();
-        for (int i = 0; i < 9; i ++) {
-            acc += rg.nextInt(10);
-        }
-        return acc;
-    }
-
-    public String getAccountHolder() {
-        return accountHolder;
-    }
-
-    public void setAccountHolder(String accountHolder) {
-        this.accountHolder = accountHolder;
-    }
+public class CurrentAccount extends BankAccount {
 
 
-    public double getBalance() {
-        return balance;
-    }
 
-    public void setBalance(double balance) {
-        this.balance = balance;
-    }
-
-
-    public boolean isHasOverdraft() {
-        return hasOverdraft;
-    }
-
-    public void setHasOverdraft(boolean hasOverdraft) {
-        this.hasOverdraft = hasOverdraft;
-    }
-
-
-    private boolean deposit(double amount){
-        if (amount > 0.0){
-            this.balance += amount;
-            return true;
-        }
-        else {
-            return false;
-        }
-    }
-
-    private boolean withdraw(double amount){
-        if (amount > 0.0){
-            this.balance -= amount;
-            return true;
-        }
-        else {
-            return false;
-        }
-    }
 }
