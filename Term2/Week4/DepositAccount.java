e5a66c507cad80a14918317c87ed0c66b8d013b7
Matthew McDougall <U1961826@unimail.hud.ac.uk>
2020-02-20 11:06:51 +0000

    Term 2

--- Term2/Week4/DepositAccount.java
+++ Term2/Week4/DepositAccount.java
@@ -6,24 +6,6 @@ public class DepositAccount {
     private double balance;
     private boolean hasOverdraft;
 
-    private boolean deposit(double amount){
-        if (amount > 0.0){
-            this.balance += amount;
-            return true;
-        }
-        else {
-            return false;
-        }
-    }
 
-    private boolean withdraw(double amount){
-        if (amount > 0.0){
-            this.balance -= amount;
-            return true;
-        }
-        else {
-            return false;
-        }
-    }
 
 }
--- Term2/Week4/StudentAccount.java
+++ Term2/Week4/StudentAccount.java
@@ -6,23 +6,5 @@ public class StudentAccount {
     private double balance;
     private boolean hasOverdraft;
 
-    private boolean deposit(double amount){
-        if (amount > 0.0){
-            this.balance += amount;
-            return true;
-        }
-        else {
-            return false;
-        }
-    }
 
-    private boolean withdraw(double amount){
-        if (amount > 0.0){
-            this.balance -= amount;
-            return true;
-        }
-        else {
-            return false;
-        }
-    }
 }
